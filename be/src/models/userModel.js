9788bfb914478d63627e996f5afa745c57713dbf
magalhaesfelipe <felipemlopes0009@gmail.com>
2025-02-13 12:11:34 -0400

    Hashing password in User model with bcryptjs.

--- be/src/models/userModel.js
+++ be/src/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-import validator from 'validator';
+import validator from "validator";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: [true, "Please enter your name"] },
@@ -7,10 +8,36 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide your email"],
     unique: true,
-    validate: [validator.isEmail, 'Please provide a valid email']
+    validate: [validator.isEmail, "Please provide a valid email"],
   },
-  password: { type: String, required: [true, 'Please provide a password'], minLength: 8 },
-  passwordConfirm: { type: String, required: [true, 'Please confirm your password'] },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minLength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same",
+    },
+  },
+});
+
+// PRE-SAVE MIDDLEWARE ON THE DOCUMENT
+userSchema.pre("save", async function (next) {
+  // Only run if password field is modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 13
+  this.password = await bcrypt.hash(this.password, 13);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 export default mongoose.model("User", userSchema);
